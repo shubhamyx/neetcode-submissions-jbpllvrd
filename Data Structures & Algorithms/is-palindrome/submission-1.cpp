454f06fb64157c633d06874ad1be43a14a58586b
Shubham Yadav <103652661+srmy123@users.noreply.github.com>
2026-03-31 15:18:49 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        
+        char i=0;
+        char j=s.size()-1;
+
+        while(i<j){
+            while(i<j && !isalnum(s[i]))i++;
+            while(i<j && !isalnum(s[j]))j--;
+
+            if(tolower(s[i])!=tolower(s[j])){
+                return false;               
+            }
+            else{
+                i++;
+                j--;
+            }
+        }
+        return true; 
+    }
+};
